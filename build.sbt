240fcbbe689b9306322f9f71f278d5373f7626cb
ChoeChangwon <qwefgh90@naver.com>
2018-08-27 14:29:49 +0900

    1) java-pty-sample

---
+++ build.sbt
@@ -0,0 +1,15 @@
+import Dependencies._
+
+lazy val root = (project in file(".")).
+  settings(
+    inThisBuild(List(
+      organization := "com.example",
+      scalaVersion := "2.12.6",
+      version      := "0.1.0-SNAPSHOT"
+    )),
+    name := "pty-sample",
+    libraryDependencies += scalaTest % Test,
+    libraryDependencies += "org.jetbrains.intellij.deps" % "pty4j" % "0.7.5" exclude("null", "purejavacomm"),
+    libraryDependencies += "com.github.purejavacomm" % "purejavacomm" % "1.0.2.RELEASE"
+  )
+resolvers += Resolver.bintrayRepo("jetbrains", "intellij-third-party-dependencies") 
---
+++ src/main/scala/example/Hello.scala
@@ -0,0 +1,38 @@
+package example
+
+import com.pty4j.{PtyProcessBuilder, PtyProcess}
+
+object Hello extends Greeting with App {
+  println(greeting)
+  // The command to run in a PTY...
+  val cmd = Array( """C:\Program Files\Java\jdk-9.0.1\bin\jshell.exe""" );
+  // The initial environment to pass to the PTY child process...
+  val env = Array[String]()//"TERM=xterm" );
+  val envs = new java.util.HashMap[String, String](System.getenv());
+  envs.put("TERM", "xterm");
+  val pty = PtyProcess.exec(cmd, envs, System.getProperty("user.home"))
+  val os = pty.getOutputStream();
+  val is = pty.getInputStream();
+  println("read input stream ")
+  new Thread{r =>{
+    while(true){
+      val b = is.read()
+      if(b > 31){
+        print(b.toChar)
+      }else
+        print("\\"+b.toInt)
+      Thread.sleep(1)
+    }
+  }}.start()
+
+  // ... work with the streams ...
+  // wait until the PTY child process terminates...
+  val result = pty.waitFor();
+
+  // free up resources.
+  pty.destroy()
+}
+
+trait Greeting {
+  lazy val greeting: String = "hello"
+}
